3a858271ff7d1abe82beeec35ee8db31add973d7
iasisalomon <iasisalomon@gmail.com>
2022-09-27 15:30:52 -0300

    initial commit

---
+++ frontend/src/features/counter/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+
+export function Counter () {
+  return (
+    <div>
+      <div className='row'>
+        <div className='col'>
+          <span>asdasdasdasdasdasd</span>
+        </div>
+      </div>
+    </div>
+  )
+}
