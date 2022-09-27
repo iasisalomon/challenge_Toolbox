1ba208ede19d71fb59cbf6f104125daf91e00ed3
iasisalomon <iasisalomon@gmail.com>
2022-09-27 09:40:41 -0300

    tests

--- api/test/files.test.js
+++ api/test/files.test.js
@@ -18,12 +18,39 @@ describe('Files', () => {
 			res.body.length.should.be.eql(7);
 		});
 	});
-	describe('/files/data', () => {
-		it('it should GET all the files', async () => {
-			const res = await chai.request(app).get('/files');
+	describe('/files/data', () =
+		let fileDownload;
+		it('should display more than one valid downloads', async () => {
+			const res = await chai.request(app).get('/files/data');
 			res.status.should.be.equal(200);
 			res.body.should.be.a('array');
-			res.body.length.should.be.eql(7);
+		});
+		it('each element should have fields with content', async () => {
+			const res = await chai.request(app).get('/files/data');
+			fileDownload = res.body;
+			fileDownload.forEach((dataDownload) => {
+				dataDownload.forEach((data) => {
+					data.should.have.property('file');
+					data.should.have.property('text');
+					data.should.have.property('number');
+					data.should.have.property('hex');
+					data.file.should.be.a('string');
+					data.text.should.be.a('string');
+					data.number.should.be.a('string');
+					data.hex.should.be.a('string');
+				});
+			});
+			res.status.should.be.equal(200);
+		});
+		it('each element should be a valid file download', async () => {
+			const res = await chai.request(app).get('/files/data');
+			fileDownload = res.body;
+			fileDownload.forEach((dataDownload) => {
+				dataDownload.forEach((data) => {
+					data.file.should.be.oneOf(['test2.csv', 'test3.csv', 'test6.csv', 'test9.csv']);
+				});
+			});
+			res.status.should.be.equal(200);
 		});
 	});
 });
